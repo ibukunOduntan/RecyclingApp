ef08c4afef58894cb7785f6dc015ed6ee4cb86eb
Oduntan Ibukun <63315272+ibukunOduntan@users.noreply.github.com>
2024-06-17 01:38:05 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,1 +1,3 @@
-gunicorn --bind=0.0.0.0 --timeout 600 application:application
+apt-get update && apt-get install -y libgl1-mesa-glx libgl1-mesa-dri
+
+gunicorn --bind=0.0.0.0 --timeout 1800 application:application
