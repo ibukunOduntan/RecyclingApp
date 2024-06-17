7b0062d6cc8e00e96587cadbf09d65853ea16d6a
Oduntan Ibukun <63315272+ibukunOduntan@users.noreply.github.com>
2024-06-17 06:48:10 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,18 @@
-apt-get update && apt-get install -y libgl1-mesa-glx libgl1-mesa-dri
+#!/bin/sh
+# This script updates the package list and installs the specified packages
+
+# Update package list
+apt-get update
+
+# Install necessary packages without recommended packages
+apt-get install -y --no-install-recommends \
+    libgl1-mesa-glx \
+    libgl1-mesa-dri \
+    libxext-dev \
+    libxrender-dev \
+    libxtst-dev
+
+# Clean up
+rm -rf /var/lib/apt/lists/*
 
 gunicorn --bind=0.0.0.0 --timeout 1800 application:application
